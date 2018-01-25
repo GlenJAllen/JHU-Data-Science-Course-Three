0b7e7ab5a4a57600cf635d8a89700b09dec0338e
GlenJAllen <glen.j.o.allen@gmail.com>
2018-01-24 19:08:59 -0500

    Reorganized code - added more extensive comments

--- run_analysis.R
+++ run_analysis.R
@@ -16,25 +16,30 @@ relevant.feature.names <- feature.names[relevant.feature.indices]
 # The labels mapping
 activity.map <- fread(file_path("activity_labels.txt"))$V2
 
-# A simple function to read in a text file from base.path/path
-# (possibly) extract certain columns
-# and set the column names for the created data frame
-read_data <- function(path, names, indices = NULL) {
-  df <- fread(file_path(path), data.table = FALSE)
-  if(!is.null(indices)) df %<>% extract(indices)
-  set_colnames(df, names)
+# read in the training and test data
+# (possibly) extract relevant columns
+# rename the columns
+# rbind train and test together
+read_and_rbind <- function(file.type, names, ...) {
+  # A simple function to read in a text file from base.path/path
+  # (possibly) extract certain columns
+  # and set the column names for the created data frame
+  .read_data <- function(path, names, indices = NULL) {
+    df <- fread(file_path(path), data.table = FALSE)
+    if(!is.null(indices)) df %<>% extract(indices)
+    set_colnames(df, names)
+  }
+
+  # get paths of the form c("train/X_train.txt", "test/X_test.txt")
+  .get_paths <- function(file.type) {s <- c("train", "test"); glue("{s}/{file.type}_{s}.txt")}
+
+  # Apply .read_data to both training and test paths from .get_paths
+  map_dfr(.get_paths(file.type), .read_data, names, ...)
 }
 
-# read in the training and test sets, subjects and activities
-# (possibly) extract relevant columns
-# rename their columns
-# (map activity codes to human-readable labels)
-# rbind train and test together for each
-# then cbind the three data frames together
-read_and_rbind <- function(file.type, names, ...) {
-  .get_paths <- function(file.type) {s <- c("train", "test"); glue("{s}/{file.type}_{s}.txt")}
-  map_dfr(.get_paths(file.type), read_data, names, ...)
-}
+# Do the above for subjects, activities, and the feature data
+# Map activities to human-readable labels
+# cbind the three together to get the final data frame
 app.data <- cbind(read_and_rbind("X", relevant.feature.names, relevant.feature.indices),
                   transmute(read_and_rbind("y", "activity"), activity = activity.map[activity]),
                   read_and_rbind("subject", "subject"))
