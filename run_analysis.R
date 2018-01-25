e420bdf3bc64a78cee00fe4ed749b5dac4fe1940
GlenJAllen <glen.j.o.allen@gmail.com>
2018-01-24 21:38:15 -0500

    formatting

--- run_analysis.R
+++ run_analysis.R
@@ -7,27 +7,27 @@ library(glue)
 setwd("location/of/the/folder/where/the/necessary/text/files/are/stored")
 
 run_analysis <- function() {
-  # read in the training and test data rename the columns rbind train and test together
+  # read in the training and test data; rename the columns; rbind train and test together
   .read_rename_rbind <- function(file.type, names) {
-    # A simple function to read in a text file and set the column names for the created data frame
+    # a simple function to read in a text file and set the column names for the created data frame
     .read_rename <- function(path, names) set_colnames(fread(path, data.table = FALSE), names)
     
     # get paths of the form c("train/X_train.txt", "test/X_test.txt")
     .get_paths <- function(file.type) {s <- c("train", "test"); glue("{s}/{file.type}_{s}.txt")}
     
-    # Apply .read_data to both training and test paths from .get_paths and rbind the results together
+    # apply .read_data to both training and test paths from .get_paths and rbind the results together
     map_dfr(.get_paths(file.type), .read_rename, names)
   }
   
-  # The names of the features for the data
+  # the names of the features for the data
   feature.names <- fread("features.txt")$V2
-  # Just the indices for the mean and std features
+  # just the indices for the mean and std features
   relevant.feature.indices <- setdiff(grep("mean|std", feature.names), grep("Freq", feature.names))
-  # The labels mapping
+  # the labels mapping
   activity.map <- fread("activity_labels.txt")$V2
   
-  # Do the above for subjects, activities, and the feature data
-  # Map activities to human-readable labels
+  # do the above for subjects, activities, and the feature data
+  # map activities to human-readable labels
   # cbind the three together to get the final data frame
   cbind(.read_rename_rbind("X", feature.names)[, relevant.feature.indices],
         transmute(.read_rename_rbind("y", "activity"), activity = activity.map[activity]),
@@ -36,7 +36,7 @@ run_analysis <- function() {
 
 app.data <- run_analysis()
 
-# Calculate the mean for each feature for each subject/activity
+# calculate the mean for each feature for each subject/activity
 app.data %>% group_by(subject, activity) %>%
   summarize_all(mean) %>%
   write.table("results.txt", row.names = FALSE)
