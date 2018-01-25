3906c80cda3cd14fd9da26ca379c42389b0f8c9e
GlenJAllen <glen.j.o.allen@gmail.com>
2018-01-24 22:30:46 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -8,11 +8,11 @@ setwd("location/of/the/folder/where/the/necessary/text/files/are/stored")
 run_analysis <- function() {
   # 1. Read in the training and test data; rename the columns; rbind train and test together
   .read_rename_rbind <- function(file.type, names) {
-    # a simple function to read in a text file and set the column names for the created data frame
+    # read in a table and set the column names
     .read_rename <- function(path, names) setNames(fread(path, data.table = FALSE), names)
     # get paths of the form c("train/X_train.txt", "test/X_test.txt")
     .get_paths <- function(file.type) {s <- c("train", "test"); glue("{s}/{file.type}_{s}.txt")}
-    # apply .read_data to both training and test paths from .get_paths and rbind the results
+    # apply .read_data to training and test paths from .get_paths and rbind the results
     map_dfr(.get_paths(file.type), .read_rename, names)
   }
   
@@ -29,7 +29,7 @@ run_analysis <- function() {
 
 app.data <- run_analysis()
 
-# calculate the mean for each feature for each subject/activity
+# 4. Calculate the mean for each feature for each subject/activity
 app.data %>% group_by(subject, activity) %>%
   summarize_all(mean) %>%
   write.table("results.txt", row.names = FALSE)
