a083fb3518db62c16cc62ec318705034e7cfc17f
GlenJAllen <glen.j.o.allen@gmail.com>
2018-01-24 21:41:22 -0500

    made comments clearer

--- run_analysis.R
+++ run_analysis.R
@@ -7,18 +7,17 @@ library(glue)
 setwd("location/of/the/folder/where/the/necessary/text/files/are/stored")
 
 run_analysis <- function() {
-  # read in the training and test data; rename the columns; rbind train and test together
+  # 1. Read in the training and test data; rename the columns; rbind train and test together
   .read_rename_rbind <- function(file.type, names) {
     # a simple function to read in a text file and set the column names for the created data frame
     .read_rename <- function(path, names) set_colnames(fread(path, data.table = FALSE), names)
-    
     # get paths of the form c("train/X_train.txt", "test/X_test.txt")
     .get_paths <- function(file.type) {s <- c("train", "test"); glue("{s}/{file.type}_{s}.txt")}
-    
     # apply .read_data to both training and test paths from .get_paths and rbind the results together
     map_dfr(.get_paths(file.type), .read_rename, names)
   }
   
+  # 2. Read in the column names and activity labels we'll need.
   # the names of the features for the data
   feature.names <- fread("features.txt")$V2
   # just the indices for the mean and std features
@@ -26,9 +25,8 @@ run_analysis <- function() {
   # the labels mapping
   activity.map <- fread("activity_labels.txt")$V2
   
-  # do the above for subjects, activities, and the feature data
-  # map activities to human-readable labels
-  # cbind the three together to get the final data frame
+  # 3. Call 1. for subjects, activities, and the feature data; map activities to human-readable labels;
+  # get rid of extraneous features; cbind the three together to get the final data frame
   cbind(.read_rename_rbind("X", feature.names)[, relevant.feature.indices],
         transmute(.read_rename_rbind("y", "activity"), activity = activity.map[activity]),
         .read_rename_rbind("subject", "subject"))
